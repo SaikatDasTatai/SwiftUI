12276d269bd3507248ab9d7e9e99ee08d539fdf3
Sd Saikat das - s0s0kh2 <Sd.Saikat.Das@walmart.com>
2023-03-21 22:36:42 +0530

    Add Hstack

--- iDine/iDine/ItemRow.swift
+++ iDine/iDine/ItemRow.swift
@@ -10,6 +10,15 @@ import SwiftUI
 struct ItemRow: View {
     let item: MenuItem
     var body: some View {
-        Text(item.name)
+        HStack {
+            Image(item.thumbnailImage)
+            Text(item.name)
+        }
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow(item: .mock)
     }
 }
