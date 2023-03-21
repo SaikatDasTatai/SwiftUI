49efedf0b4fb891d2f6fe570d35f161fdb4a050a
Sd Saikat das - s0s0kh2 <Sd.Saikat.Das@walmart.com>
2023-03-22 00:35:19 +0530

    Add Item Detail page

--- iDine/iDine/ItemRow.swift
+++ iDine/iDine/ItemRow.swift
@@ -17,35 +17,40 @@ struct ItemRow: View {
         "V": .green
     ]
     var body: some View {
-        HStack {
-            Image(item.thumbnailImage)
-                .clipShape(Circle())
-                .overlay(
-                    Circle().stroke(
-                        .gray,
-                        lineWidth: 2
-                    )
-                )
-            
-            VStack(alignment: .leading) {
-                Text(item.name)
-                    .font(.headline)
-                Text("$\(item.price)")
-            }
-            
-            Spacer()
-            
-            ForEach(
-                item.restrictions,
-                id: \.self
-            ) { restriction in
-                Text(restriction)
-                    .font(.caption)
-                    .fontWeight(.black)
-                    .padding(5)
-                    .background(colors[restriction, default: .black])
+        
+        NavigationLink {
+            Text(item.name)
+        } label: {
+            HStack {
+                Image(item.thumbnailImage)
                     .clipShape(Circle())
-                    .foregroundColor(.white)
+                    .overlay(
+                        Circle().stroke(
+                            .gray,
+                            lineWidth: 2
+                        )
+                    )
+                
+                VStack(alignment: .leading) {
+                    Text(item.name)
+                        .font(.headline)
+                    Text("$\(item.price)")
+                }
+                
+                Spacer()
+                
+                ForEach(
+                    item.restrictions,
+                    id: \.self
+                ) { restriction in
+                    Text(restriction)
+                        .font(.caption)
+                        .fontWeight(.black)
+                        .padding(5)
+                        .background(colors[restriction, default: .black])
+                        .clipShape(Circle())
+                        .foregroundColor(.white)
+                }
             }
         }
     }
