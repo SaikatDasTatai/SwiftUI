cdecc8853cd87021a9ebda9495af4bd3da515625
Sd Saikat das - s0s0kh2 <Sd.Saikat.Das@walmart.com>
2023-03-21 14:41:23 +0530

    Add pre requisite files

---
+++ iDine/iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Sd Saikat Das on 21/03/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
