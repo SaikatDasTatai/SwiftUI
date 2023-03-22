682c2b15fe7a03641c4d4986bcd122745fe45023
Sd Saikat das - s0s0kh2 <Sd.Saikat.Das@walmart.com>
2023-03-22 14:04:38 +0530

    Add confirm order alert

--- iDine/iDine/CheckoutView.swift
+++ iDine/iDine/CheckoutView.swift
@@ -18,6 +18,14 @@ struct CheckoutView: View {
     let tipAmounts = [10, 15, 20, 25, 0]
     @State private var tipAmount = 15
     
+    @State private var showPaymentAlert = false
+    
+    var totalPrice: String {
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+        return (total + tipValue).formatted(.currency(code: "USD"))
+    }
+    
     var body: some View {
         Form {
             Section {
@@ -42,15 +50,20 @@ struct CheckoutView: View {
                     .pickerStyle(.segmented)
                 }
                 
-                Section("Total: $100") {
+                Section("Total: \(totalPrice)") {
                     Button("Confirm Order") {
-                        
+                        showPaymentAlert.toggle()
                     }
                 }
             }
         }
         .navigationTitle("Payment")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Order confirmed", isPresented: $showPaymentAlert) {
+            
+        } message: {
+            Text("Your total was \(totalPrice) - thank you")
+        }
     }
 }
 
